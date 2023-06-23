6dc494e855df8618cfee59be74c84fdc1ec40a74
booksey <booksey87@gmail.com>
2023-06-23 16:15:08 +0200

    new hetimenu

--- hetimenu.php
+++ hetimenu.php
@@ -2,40 +2,40 @@
 require_once 'header.php';
 
 $dailyMenu = [
-    '2023.Június 12. hétfő' => [
-        'Kertész leves',
-        'Sertésszelet Jóasszony módra pirított burgonyával',
-        'Isler',
+    '2023.Június 19. hétfő' => [
+        'Palóc leves',
+        'Káposztás tészta',
+        'Linzer karika',
     ],
-    '2023.Június 13. kedd' => [
-        'Orja leves',
-        'Lecsós szelet rizzsel',
-        'Tiramisu',
+    '2023.Június 20. kedd' => [
+        'Zöldségkrémleves',
+        'Kijevi csirkemell burgonyapürével',
+        'Lekváros palacsinta',
     ],
-    '2023.Június 14. szerda' => [
-        'Francia hagymaleves',
-        'Bolognai spagetti',
+    '2023.Június 21. szerda' => [
+        'Tárkonyos lencseleves virslivel',
+        'Natur sertésszelet párolt zöldségekkel',
+        'Puncs szelet'
+    ],
+    '2023.Június 22. csütörtök' => [
+        'Daragaluska leves',
+        'Tejfölös,majorannás burgonyafőzelék sült kolbásszal',
+        'Gesztenye püré'
+    ],
+    '2023.Június 23. péntek' => [
+        'Görög gyümölcsleves',
+        'Rántott cukkini rizzsel,tartárral',
+        'Mascarponés,citromos pohárkrém'
+    ],
+    '2023.Június 24. szombat' => [
+        'Csülkös bableves',
+        'Tojásos galuska',
+        'Fejes saláta'
+    ],
+    '2023.Június 25. vasárnap' => [
+        'Újházy tyúkhúsleves',
+        'Csirkepörkölt szarvacska tésztával',
         'Borpatika kehely'
-    ],
-    '2023.Június 15. csütörtök' => [
-        'Erőleves fridatto',
-        'Töltött paprika sós burgonyával',
-        'Kókusz golyó'
-    ],
-    '2023.Június 16. péntek' => [
-        'Karfiol leves',
-        'Csibe brassói kocka burgonyával',
-        'Cékla saláta'
-    ],
-    '2023.Június 17. szombat' => [
-        'Májgombóc leves',
-        'Mátrai borzaska rizibizivel',
-        'Túrógombóc'
-    ],
-    '2023.Június 18. vasárnap' => [
-        'Kolbászos burgonyaleves',
-        'Csikós tokány galuskával',
-        'Linzer karika'
     ],
 ];
 ?>
@@ -44,7 +44,7 @@ $dailyMenu = [
         <div class="container-md mt-5" data-aos="fade-in">
             <div class="d-flex flex-row justify-content-center">
                 <div>
-                    <h1 class="text-center">Heti menü (24.hét) <br />1890.-Ft / nap</h1>
+                    <h1 class="text-center">Heti menü (25.hét) <br />1890.-Ft / nap</h1>
                     <br />
                     <?php
                     foreach ($dailyMenu as $day => $menus) { ?>
