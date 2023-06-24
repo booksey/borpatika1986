d70c354f96044c71ffbecbcb6a3d8c13e65ed9c0
booksey <booksey87@gmail.com>
2023-06-24 14:40:27 +0200

    itallap table responsive fixes

--- itallap.php
+++ itallap.php
@@ -2,7 +2,6 @@
 require_once 'header.php';
 require_once 'drinks.php';
 
-//TODO: scrollbar always show on top and bottom
 ?>
 
 <main id="main">
@@ -20,64 +19,71 @@ require_once 'drinks.php';
                     <?php foreach ($menus['menus'] as $menu) {
                         $menuId = "menuId" . $menuCnt;
                     ?>
-                        <div class="accordion-item italitem">
-                            <button class="accordion-button italbutton" type="button" data-bs-toggle="collapse" data-bs-target="#<?= $menuId; ?>" aria-expanded="true" aria-controls="<?= $menuId; ?>">
+                        <div class="accordion-item drinkItem">
+                            <button class="accordion-button drinkButton" type="button" data-bs-toggle="collapse" data-bs-target="#<?= $menuId; ?>" aria-expanded="true" aria-controls="<?= $menuId; ?>">
                                 <?= ($menu['mainTitle'] ? $menu['mainTitle'] : '') ?>
                             </button>
                             <div id="<?= $menuId; ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= $menuId; ?>" data-bs-parent="#itallap">
                                 <div class="accordion-body">
-                                    <div class="table table-borderless table-responsive-md">
-                                        <table class="table">
-                                            <tbody>
-                                                <?php
-                                                //4 columns default
-                                                $firstDiv = '4';
-                                                $secondDiv = '2';
-                                                switch (count($menu['columns'])) {
-                                                    case 5:
-                                                        $firstDiv = '4';
-                                                        $secondDiv = '2';
-                                                        break;
-                                                    case 3:
-                                                        $firstDiv = '6';
-                                                        $secondDiv = '2';
-                                                        break;
-                                                    case 2:
-                                                        $firstDiv = '8';
-                                                        $secondDiv = '2';
-                                                        break;
-                                                    case 1:
-                                                        $firstDiv = '10';
-                                                        $secondDiv = '2';
-                                                        break;
-                                                }
-                                                ?>
-                                                <tr>
-                                                    <td scope="row" class="col-<?php echo $firstDiv; ?> border-0"></td>
-                                                    <?php foreach ($menu['columns'] as $column) { ?>
-                                                        <td class="col-<?php echo $secondDiv; ?> border-0 text-center">
-                                                            <?php echo $column; ?>
-                                                        </td>
-                                                    <?php } ?>
-                                                </tr>
-
-                                    </div>
-                                    <div class="table table-borderless table-responsive-md">
-                                        <?php foreach ($menu['drinks'] as $name => $con) { ?>
+                                    <table class="table table-mobile-responsive">
+                                        <thead>
+                                            <?php
+                                            //4 columns default
+                                            $firstDiv = '4';
+                                            $secondDiv = '2';
+                                            switch (count($menu['columns'])) {
+                                                case 5:
+                                                    $firstDiv = '4';
+                                                    $secondDiv = '2';
+                                                    break;
+                                                case 3:
+                                                    $firstDiv = '6';
+                                                    $secondDiv = '2';
+                                                    break;
+                                                case 2:
+                                                    $firstDiv = '8';
+                                                    $secondDiv = '2';
+                                                    break;
+                                                case 1:
+                                                    $firstDiv = '10';
+                                                    $secondDiv = '2';
+                                                    break;
+                                            }
+                                            $columns = [
+                                                0 => '',
+                                            ];
+                                            ?>
                                             <tr>
-                                                <td class="col-<?php echo $firstDiv; ?> text-start border-0">
-                                                    <?php echo $name; ?>
-                                                </td>
-                                                <?php foreach ($con as $price) { ?>
-                                                    <td class="col-<?php echo $secondDiv; ?> text-center border-0">
-                                                        <?php echo $price; ?>.-
-                                                    </td>
+                                                <th class="col-<?php echo $firstDiv; ?> border-0" scope="col"></th>
+                                                <?php foreach ($menu['columns'] as $column) { ?>
+                                                    <th class="col-<?php echo $secondDiv; ?> border-0 text-center" scope="col">
+                                                        <?php
+                                                        echo $column;
+                                                        $columns[] = $column;
+                                                        ?>
+                                                    </th>
                                                 <?php } ?>
                                             </tr>
-                                        <?php } ?>
+                                        </thead>
+                                        <tbody>
+
+                                            <?php foreach ($menu['drinks'] as $name => $con) {
+                                                $columnCnt = 1;
+                                            ?>
+                                                <tr>
+                                                    <td data-content="" class="col-<?php echo $firstDiv; ?> border-0" scope="row">
+                                                        <?php echo $name; ?>
+                                                    </td>
+                                                    <?php foreach ($con as $price) { ?>
+                                                        <td data-content="<?= $columns[$columnCnt]; ?>" class="col-<?php echo $secondDiv; ?> border-0 text-center">
+                                                            <?php echo $price; ?>.-
+                                                        </td>
+                                                    <?php $columnCnt++;
+                                                    } ?>
+                                                </tr>
+                                            <?php } ?>
                                         </tbody>
-                                        </table>
-                                    </div>
+                                    </table>
                                 </div>
                             </div>
                         <?php $menuCnt++;
