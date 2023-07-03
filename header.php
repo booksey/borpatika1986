b82b90c0193e107dc00b02ccd63fea141d7eae1e
booksey <booksey87@gmail.com>
2023-07-03 22:20:46 +0200

    https redirect cleared from code

--- header.php
+++ header.php
@@ -1,11 +1,3 @@
-<?php
-if ((empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") && $_SERVER['HTTP_HOST'] != 'localhost') {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
-}
-?>
 <!DOCTYPE html>
 <html lang="hu">
 
