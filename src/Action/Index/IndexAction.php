551b7ba728763f2bcc00bc1598d81c126f8b56af
booksey <booksey87@gmail.com>
2023-08-06 11:24:32 +0200

    refactor

--- src/Action/Index/IndexAction.php
+++ src/Action/Index/IndexAction.php
@@ -5,17 +5,24 @@ declare(strict_types=1);
 namespace App\Action\Index;
 
 use App\Action\AbstractAction;
-use Laminas\Diactoros\Response\HtmlResponse;
 use Psr\Http\Message\ResponseInterface;
+use Twig\Environment;
 
 class IndexAction extends AbstractAction
 {
-    public function __construct()
+    public function __construct(private readonly Environment $twig)
     {
     }
 
     public function invoke(): ResponseInterface
     {
-        return new HtmlResponse('Index', 200);
+        $this->response->getBody()->write($this->twig->render(
+            'index.html.twig',
+            [
+                'language' => 'hu',
+                'cookieFooterDisplayClass' => 'd-block'
+            ]
+        ));
+        return $this->response;
     }
 }
